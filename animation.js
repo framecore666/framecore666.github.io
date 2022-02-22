3c1fe1fb9f9728555dcb01f4cd5e30bab7d9b527
framecore666 <100089031+framecore666@users.noreply.github.com>
2022-02-22 13:01:40 +0600

    Delete animation.js

--- animation.js
+++
@@ -1,9 +0,0 @@
-alert();
-
-const hero = document.querySelector('.hero');
-const slider = document.querySelector('.slider');
-const headline = document.querySelector('.headline');
-
-const tl = new Time();
-
-tl.fromTo(hero,1,{height: "0%"}, {height: '80%'});
